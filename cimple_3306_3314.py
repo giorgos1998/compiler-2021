36a05dadc2dc5653e6853abf5ce3d829964dbd2c
George Papadatos <george.papadatos98@gmail.com>
2021-03-07 13:31:48 +0200

    Finished lexical analyzer, Added example programs

--- cimple_3306_3314.py
+++ cimple_3306_3314.py
@@ -3,23 +3,188 @@
 
 # Thanasi check oti einai swsta ta apo panw
 
-# Module to read command-line arguments
 import sys
+import string
+
+letters = string.ascii_letters
+numbers = string.digits
+keywords = ["program","declare","if","else","while","switchcase","forcase","incase","case","default",
+            "not","and","or","function","procedure","call","return","in","inout","input","print"]
+lineCounter = 1
 
 # Token class used by lexical analyzer
 class Token:
-    def __init__(self, type, content):
-        self.type = type
+    def __init__(self, tkType, content, line):
+        self.tkType = tkType
         self.content = content
+        self.line = line
 
 # Displays error message to user with required info
-# def errorHandler():
+def errorHandler(message):
+    exitMessage = "ERROR at line " + str(lineCounter) + ": " + message
+    sys.exit(exitMessage)
 
 # Lexical Analyzer
-# def lexAn():
-#     currentState = ""
+def lexAn():
+
+    state = "start"
+    idSize = 0
+    content = ""
+    tkType = ""
+
+    while True:
+
+        lastPosition = sourceFile.tell()
+        buffer = sourceFile.read(1)
+
+        # reached EOF
+        if buffer == "":
+            errorHandler("Compiler reached end of file before program terminator (.)")
+
+        # comment handling
+        elif state == "start" and buffer == "#":
+            state = "comment"
+        
+        elif state == "comment":
+            if buffer == "#":
+                state = "start"
+                
+        # whitespace handling
+        elif state == "start" and buffer in string.whitespace:
+            
+            # keeps track of current line
+            if buffer == "\n":
+                global lineCounter
+                lineCounter += 1
+
+
+        # identifier handling
+        elif state == "start" and buffer in letters:
+            state = "identifier"
+            tkType = "identifier"
+            idSize += 1
+            content += buffer
+        
+        elif state == "identifier":
+            if idSize > 30:
+                errorHandler("Cannot have identifier larger than 30 characters.")
+            if buffer in letters or buffer in numbers:
+                idSize += 1
+                content += buffer
+            else:
+                sourceFile.seek(lastPosition)
+                break
+        
+        # number handling
+        elif state == "start" and buffer in numbers:
+            state = "number"
+            tkType = "number"
+            content += buffer
+        
+        elif state == "number":
+            if buffer in numbers:
+                content += buffer
+            else:
+                sourceFile.seek(lastPosition)
+                break
+        
+        # operator handling
+        elif state == "start" and buffer in "+-":
+            content = buffer
+            tkType = "addOp"
+            break
+
+        elif state == "start" and buffer in "*/":
+            content = buffer
+            tkType = "mulOp"
+            break
+
+        # assignment handling
+        elif state == "start" and buffer == ":":
+            state = "assignment"
+            tkType = "assignment"
+            content += buffer
+        
+        elif state == "assignment":
+            if buffer == "=":
+                content += buffer
+                break
+            else:
+                errorHandler("Invalid syntax, expected '=' after ':'")
+
+        # relation operator handling
+        elif state == "start" and buffer == "<":
+            state = "smallerOp"
+            tkType = "relOp"
+            content += buffer
+        
+        elif state == "smallerOp":
+            if buffer == "=":
+                content += buffer
+                break
+            elif buffer == ">":
+                content += buffer
+                break
+            else:
+                sourceFile.seek(lastPosition)
+                break
+
+        elif state == "start" and buffer == ">":
+            state = "greaterOp"
+            tkType = "relOp"
+            content += buffer
+
+        elif state == "greaterOp":
+            if buffer == "=":
+                content += buffer
+                break
+            else:
+                sourceFile.seek(lastPosition)
+                break
+
+        elif state == "start" and buffer == "=":
+            tkType = "relOp"
+            content = buffer
+            break
+
+        # delimiter handling
+        elif state == "start" and buffer in ",;":
+            tkType = "delimiter"
+            content = buffer
+            break
+
+        # group symbol handling
+        elif state == "start" and buffer in "[]()}{":
+            tkType = "groupSymbol"
+            content = buffer
+            break
+
+        # program termination handling
+        elif state == "start" and buffer == ".":
+            tkType = "terminator"
+            content = buffer
+
+            # check for additional code after program end
+            localBuffer = sourceFile.read(1)
+            while localBuffer != "":
+                if localBuffer not in string.whitespace:
+                    print("WARNING: Additional code found after program end. Additional code will be ignored.")
+                    break
+                localBuffer = sourceFile.read(1)
+            
+            break
+
+        # unknown character handling
+        else:
+            errorHandler("Character '" + buffer + "' is not supported in C-imple.")
+
+    # check if identifier is a keyword
+    if tkType == "identifier" and content in keywords:
+        tkType = "keyword"
     
-#     return word
+    # create and return token
+    token = Token(tkType, content, lineCounter)
+    return token
 
 # Gets file to compile
 try:
@@ -34,4 +199,11 @@ else:
     if sys.argv[1][-3:] != ".ci":
         sys.exit("Error: Source file must be a C-imple file (.ci)")
 
+token = lexAn()
+while token.tkType != "terminator":
+    print("LINE: " + ("%-10s" % lineCounter) + "TYPE: " + ("%-17s" % token.tkType) + "TOKEN:", token.content)
+    token = lexAn()
+print("LINE: " + ("%-10s" % lineCounter) + "TYPE: " + ("%-17s" % token.tkType) + "TOKEN:", token.content)
+
+sourceFile.close()
 print("program finished")
