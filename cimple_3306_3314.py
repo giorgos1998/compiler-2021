ab17c3a09bdb188a5df379898caf35e5788d775a
George Papadatos <george.papadatos98@gmail.com>
2021-04-25 21:02:41 +0300

    Added dummy subroutines for quads, added quad code
    Quad code for arithmetic and bool expressions

--- cimple_3306_3314.py
+++ cimple_3306_3314.py
@@ -25,6 +25,26 @@ def errorHandler(message):
     exitMessage = "ERROR at line " + str(lineCounter) + ": " + message
     sys.exit(exitMessage)
 
+
+def genquad(par1, par2, par3, par4):
+    pass
+
+def newTemp():
+    return 0
+
+def backpatch(par1, par2):
+    pass
+
+def nextquad():
+    return 0
+
+def merge(par1, par2):
+    return (0,0)
+
+def makelist(par1):
+    return 0
+
+
 # Lexical Analyzer
 def lexAn():
 
@@ -212,10 +232,14 @@ def MUL_OP():
 
 def INTEGER():
     global token
+    number = token.content
     if token.tkType == "number":
         token = lexAn()
     else:
         errorHandler(token.content + " is not of type int, expected integer")
+    # will not reach return statement if there is a problem with the number
+    return number
+    
 
 def ID():
     global token
@@ -265,14 +289,19 @@ def idtail():
         # ')' is checked in actualparlist()
 
  
-
+# bfactTF is a list that contains the True and False lists for the whole boolfactor
+# (bfactTF[0] = R.true, bfactTF[1] = R.false)
+# we get the results with the return values, not with parameters
 def boolfactor():
     global token
+    bfactTF = []
     if token.tkType == "keyword" and token.content == "not":
         token = lexAn()
         if token.tkType == "groupSymbol" and token.content == "[":
             token = lexAn()
-            condition()
+            # get condition lists and reverse them
+            bfactTF_reversed = condition()
+            bfactTF = [bfactTF_reversed[1], bfactTF_reversed[0]]
             if token.tkType == "groupSymbol" and token.content == "]":
                 token = lexAn()
             else:
@@ -281,62 +310,119 @@ def boolfactor():
             errorHandler("Missing '[' after 'not' keyword")
     elif token.tkType == "groupSymbol" and token.content == "[":
         token = lexAn()
-        condition()
+        # get condition lists and pass them to caller
+        bfactTF = condition()
         if token.tkType == "groupSymbol" and token.content == "]":
             token = lexAn()
         else:
             errorHandler("Missing ']' at the end of a bool expression")
     else:
-        expression()
+        E1result = expression()
+        # keep token before checking
+        relop = token.content
         REL_OP()
-        expression()
-        
+        E2result = expression()
 
+        # create and save quad that must be filled with jump address in case of True
+        bfactTF[0] = makelist(nextquad())
+        genquad(relop, E1result, E2result, "_")
+        # create and save quad that must be filled with jump address in case of False
+        bfactTF[1] = makelist(nextquad())
+        genquad("jump", "_", "_", "_")
+
+    # return boolfactor result to caller
+    return bfactTF
+
+        
+# btermTF is a list that contains the True and False lists for the whole boolterm
+# (btermTF[0] = Q.true, btermTF[1] = Q.false)
+# we get the results with the return values, not with parameters
 def boolterm():
     global token
-    boolfactor()
+    btermTF = boolfactor()
     while token.tkType == "keyword" and token.content == "and":
+        # backpatch True list with next quad (if true, check next 'and' condition)
+        backpatch(btermTF[0], nextquad())
+        # bfactTF is a list that contains the True and False lists of the 2nd boolfactor (R2)
         token = lexAn()
-        boolfactor()
+        bfactTF = boolfactor()
+        # get all False lists that we don't know where to jump yet
+        btermTF[1] = merge(btermTF[1], bfactTF[1])
+        # btermTF[0] now contains the only True quad that must be backpatched later
+        btermTF[0] = bfactTF[0]
+
+    # return boolterm result lists to caller
+    return btermTF
 
 def factor():
     global token
+    result = ""
     if token.tkType == "number":
-        INTEGER()
+        result = INTEGER()
     elif token.tkType == "groupSymbol" and token.content == "(":
         token = lexAn()
-        expression()
+        result = expression()
         if token.tkType == "groupSymbol" and token.content == ")":
             token = lexAn()
         else:
             errorHandler("Missing ')' at the end of an arithmetic expression")
     else:
+        # NOTE result is function name here, not sure what to do
+        result = token.content
         ID()
         idtail()
+    return result
         
 
+# F1result = F1.place (result of 1st factor), F2result = F2.place (result of 2nd factor)
+# we get the results with the return values, not with parameters
 def term():
     global token
-    factor()
+    F1result = factor()
     while token.tkType == "mulOp":
         MUL_OP()
-        factor()
+        F2result = factor()
+        w = newTemp()
+        genquad("x", F1result, F2result, w)
+        F1result = w
+    return F1result
           
 
+# T1result = T1.place (result of 1st temp), T2result = T2.place (result of 2nd temp)
+# we get the results with the return values, not with parameters
 def expression():
     global token
+    # NOTE sign will need some special handling
     optionalSign()
-    term()
+    T1result = term()
     while token.tkType == "addOp":
         ADD_OP()
-        term()
+        T2result = term()
+        w = newTemp()
+        genquad("+", T1result, T2result, w)
+        T1result = w
+    return T1result
 
+
+# condTF is a list that contains the True and False lists for the whole condition
+# (condTF[0] = B.true, condTF[1] = B.false)
+# we get the results with the return values, not with parameters
 def condition():
     global token
-    boolterm()
+    condTF = boolterm()
     while token.tkType == "keyword" and token.content == "or":
+        # backpatch false list with the next quad (if false, check next 'or' condition)
+        backpatch(condTF[1], nextquad())
+        # btermTF is a list that contains the True and False lists of the 2nd boolterm (Q2)
         token = lexAn()
-        boolterm()
+        btermTF = boolterm()
+        # get all True lists that we don't know where to jump yet
+        condTF[0] = merge(condTF[0], btermTF[0])
+        # condTF[1] now contains the only False quad that must be backpatched later 
+        condTF[1] = btermTF[1]
+    
+    # return condition result lists to caller
+    return condTF
 
 def elsepart():
     global token
@@ -542,11 +628,12 @@ def declarations():
 
 def subprogram():    
     global token
+    funcName = token.content
     ID()
     if token.tkType == "groupSymbol" and token.content == "(":
         token = lexAn()
         formalparlist()
-        block()
+        block(funcName, False)
     else:
         errorHandler("Missing '(' at function/procedure declaration")
 
@@ -614,19 +701,25 @@ def subprograms():
         subprogram()
         
         
-def block():
+def block(programName, isMain):
     global token
     declarations()
     subprograms()
+    genquad("begin_block", programName, "_", "_")
     statements()
+    # block() is common for main and other functions, so we need isMain to know if we must add 'halt'
+    if isMain:
+        genquad("halt", "_", "_", "_")
+    genquad("end_block", programName, "_", "_")
 
 
 def program():
     global token
     if token.tkType == "keyword" and token.content == "program":
         token = lexAn()
+        programName = token.content
         ID()
-        block()
+        block(programName, True)
         if token.tkType == "terminator":
             print("No syntax errors found")
         else:
@@ -639,7 +732,9 @@ def synAn():
     global token
     program()
         
-    
+
+################################### Main ###################################
+
 # Gets file to compile
 try:
     sourceFile = open(sys.argv[1])
